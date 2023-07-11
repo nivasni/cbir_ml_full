b29d32d1fb52e5f2bc04a33a0b008620baa9c43e
nivasni <78199000+nivasni@users.noreply.github.com>
2023-07-11 15:44:42 +0530

    Initial commit

---
+++ src/cbirmld/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cbirmld;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mf=new MainFrame();
+        mf.setVisible(true);
+        mf.setTitle("MLDL Image Annotation");
+        mf.setResizable(false);
+    }
+    
+}
